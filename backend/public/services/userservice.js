b666a344ced841743341822919ca49c5f82877ac
SzAndras1 <szbandras2@gmail.com>
2023-06-02 03:16:15 +0200

    Extract user validation to userservice

--- backend/public/services/userservice.js
+++ backend/public/services/userservice.js
@@ -1,3 +1,20 @@
-//const express = require('express');
-//const router = express.Router();
-//router.use(express.json());
+const registerUser = function (obj, callback) {
+    if (obj.username === undefined || obj.password === undefined) {
+        return callback(Error('Username or password missing.'));
+    }
+
+    if (obj.username.length < 3 || obj.password.length < 3) {
+        return callback(Error('Username or password length is less than 3 characters.'));
+    }
+
+    const regex = /^[a-zA-Z0-9]+$/
+    if (!obj.username.match(regex)) {
+        return callback(Error('Wrong character included.'));
+    }
+
+    return callback(null, 'Successful');
+}
+
+module.exports = registerUser;
+
+
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const insert = require("../public/javascripts/db-operations");
+const registerUser = require("../public/services/userservice");
 const router = express.Router();
 router.use(express.json());
 const collection = 'user'
 
 /** POST register */
-router.post('/register', function(req, res, next){
-    let toRegisterUser = { username: req.body['username'], password: req.body['password'] };
-    if(toRegisterUser.username === undefined || toRegisterUser.password === undefined){
-        return res.status(400).end('Username or password missing.');
-    }
-    if(toRegisterUser.username.length < 3 || toRegisterUser.password.length < 3){
-        return res.status(400).end('Username or password length is less than 3 characters.');
-    }
-    const regex = /^[a-zA-Z0-9]+$/
-    if(!toRegisterUser.username.match(regex)){
-        return res.status(400).end('Wrong character included.');
-    }
-    insert(collection, toRegisterUser)
-    toRegisterUser.message = 'Successful registration!'
-    return res.status(201).json(toRegisterUser);
+router.post('/register', function (req, res, next) {
+    let toRegisterUser = {username: req.body['username'], password: req.body['password']};
+    registerUser(toRegisterUser, function (err, result) {
+        if (err)
+            return res.status(400).end(err.message, err.stack);
+        else {
+            insert(collection, toRegisterUser)
+            //toRegisterUser.message = 'Successful registration!'
+            return res.status(201).json(toRegisterUser);
+        }
+    })
 });
 
 module.exports = router;
