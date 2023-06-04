40201bd600bba71bd00bef0af912a00b4a588f63
SzAndras1 <szbandras2@gmail.com>
2023-06-04 23:46:53 +0200

    Small refactor to login and register in UI

--- frontend/src/app/user-login/user-login.component.ts
+++ frontend/src/app/user-login/user-login.component.ts
@@ -8,7 +8,7 @@ import {catchError, throwError} from "rxjs";
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.scss']
+  styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent implements OnInit {
 
--- frontend/src/app/user-register/user-register.component.ts
+++ frontend/src/app/user-register/user-register.component.ts
@@ -8,7 +8,7 @@ import {Router} from "@angular/router";
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
-  styleUrls: ['./user-register.component.scss']
+  styleUrls: ['./user-register.component.css']
 })
 export class UserRegisterComponent implements OnInit{
 
